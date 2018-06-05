f6e165f7543d3118afbbaa1e568cf0a2afd124fe
Colin Osborn <colin@Colins-MacBook-Pro.local>
2018-06-05 17:19:02 -0600

    updating files

---
+++ wp-content/uploads/bb-plugin/cache/16-layout-partial.js
@@ -0,0 +1,2 @@
+
+jQuery(function($){$(function(){$('.fl-node-5b15cb458b292 .fl-photo-img').on('mouseenter',function(e){$(this).data('title',$(this).attr('title')).removeAttr('title');}).on('mouseleave',function(e){$(this).attr('title',$(this).data('title')).data('title',null);});});});
